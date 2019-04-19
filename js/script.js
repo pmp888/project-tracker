ef927f8b14defa2ffea2ce6f628099a9ece4edb6
rjustin <ryan.justin@ymail.com>
2019-04-19 17:23:53 +0800

    Added API to call backend

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+const url = 'http://localhost:3000';
+
 $(function () {
     $('#datetimepicker1').datetimepicker();
     $('#datetimepicker2').datetimepicker();
@@ -7,5 +9,19 @@ $(function () {
         console.log($('#task-start-time').val());
         console.log($('#task-end-time').val());
         console.log($('input[name=task-status]:checked').val());
+
+        $.ajax({
+            url: url + '/task',
+            method: 'POST',
+            data: JSON.stringify({
+                task_name: $('#task-name').val(),
+                task_status: $('#task_status').val()
+            }),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json'
+          }).done(function(data) {
+              console.log('data', data);
+            $('#status').innerHTML('Task saved!');
+          });
     });
 });
