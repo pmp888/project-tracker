8a6ce1c01e0ad1f5784f2317284d0480f0514d23
Pedro Miguel <pedrom888@gmail.com>
2019-04-22 15:23:35 +0700

    add task_update controller

---
+++ server/controllers/task_update.js
@@ -0,0 +1,23 @@
+// Import message
+const task = require('../model/task')
+
+module.exports = function(req, res, next) {
+	message.findByIdAndUpdate(req.params.id,
+		req.body,
+		{
+			task_name:{
+        type: String,
+      },
+      time_start:{
+        type: Date,
+      },
+      time_end:{
+        type: Date,
+      },
+      task_status:{
+        type: Boolean,
+      }
+		}).then(function(updated_task) {
+		res.status(200).json(updated_task)
+	})
+}
