1aac740a645871960c63e9b682620f8b31b536ad
Michelle Loges <michelleloges.aiesec@gmail.com>
2019-04-22 15:42:35 +0700

    task route added

--- server/routes.js
+++ server/routes.js
@@ -6,6 +6,7 @@ module.exports = function(app) {
 
   	app.post('/task', require('./controllers/task_create'))
     app.get('/task', require('./controllers/tasks_read'))
+    app.get('/task/:id', require('./controllers/task_read'))
     app.patch('/task/:id', require('./controllers/task_update'))
 
 }
