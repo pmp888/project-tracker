90c82effe2fef4bcbbee9e20a5eca7af8e0f47c8
Pedro Miguel <pedrom888@gmail.com>
2019-04-22 15:10:18 +0700

    fixed /model/ foldername

--- server/controllers/tasks_read.js
+++ server/controllers/tasks_read.js
@@ -1,8 +1,8 @@
 // Import db_channel
-const db_task = require('../models/task')
+const db_task = require('../model/task')
 
 module.exports = function(req, res, next) {
-	db_channel.find({}).then(function(tasks) {
+	db_task.find({}).then(function(tasks) {
 		res.json(tasks)
 	})
 }
